baa3e05dfc8655dd42965aa30358b6108f999f67
aspyker <awspyker@gmail.com>
2013-07-01 21:18:59 -0400

    add control of jmeter through ssh

---
+++ acmeair-driver-control/cloud.groovy
@@ -0,0 +1,8 @@
+cloud {
+	//hardCodedJMeterInstanceList = ['1.1.1.1']
+	hardCodedJMeterInstanceList = ['1.1.1.1', '1.1.1.2']
+	//elbOrDirectToAppServerInstanceListForJMeterHosts = ['xxx.us-east-1.elb.amazonaws.com']
+	elbOrDirectToAppServerInstanceListForJMeterHosts = ['2.1.1.1', '2.1.1.2']
+	user = "ec2-user"
+	keyFile = "/pathto/acmeair-netflix.pem"
+}
